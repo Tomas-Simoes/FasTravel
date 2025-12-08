a11b970998f646cfe5a06d297d526b35b9549672
Tomas-Simoes <tomas20simoes@gmail.com>
2025-12-08 18:09:56 +0000

    Update users_controller.rb

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -2,33 +2,20 @@ class UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create 
-      user = User.create!(user_params)
-      @token = encode_token(user_id: user.id)
-
-      render json: {
-          user: UserSerializer.new(user), 
-          token: @token
-      }, status: :created
-  rescue ActiveRecord::RecordInvalid => e 
-    render json: { errors: e.record.errors.full_messages}, status: :unprocessable_entity
-  end
-
-  def show 
-    user = User.find(params[:id])
-    
-    if current_user.id == user.id
-      render json: UserSerializer.new(user)
+    user = User.new(user_params)
+    if user.save
+      token = encode_token(user_id: user.id)
+      render json: { user: UserSerializer.new(user), token: token }, status: :created
     else
-        render json: {message: 'Unauthorized'}, status: :forbidden
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
-
-  rescue ActiveRecord::RecordNotFound
-    render json: {message: "User not found"}, status: :not_found
+  rescue ActionController::ParameterMissing => e
+    render json: { errors: ["Param missing: #{e.param}"] }, status: :bad_request
   end
 
   private
 
   def user_params 
-      params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password)
   end
 end
