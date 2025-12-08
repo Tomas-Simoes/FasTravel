15b61f2dbf3433c2eb4af4e52ead7d1977fd2873
Tomas-Simoes <tomas20simoes@gmail.com>
2025-12-08 18:17:36 +0000

    more bug fix in backend

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: URI::MailTo::EMAIL_REGEXP}
   validates :password, length: {minimum: 8}, if: :password_required? 
+  validates :name, presence: true                
   has_many :histories
 
   before_save :downcase_email
